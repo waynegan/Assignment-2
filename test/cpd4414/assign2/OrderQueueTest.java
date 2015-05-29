2069802298910d9262c79d030cbfe16426100763
wayne <wayne@wayne-PC>
2015-05-29 15:26:05 -0400

    implemented testGetNextWhenNoOrdersInSystemThenReturnNull()

--- test/cpd4414/assign2/OrderQueueTest.java
+++ test/cpd4414/assign2/OrderQueueTest.java
@@ -115,16 +115,10 @@ public class OrderQueueTest {
     @Test 
     public void testGetNextWhenNoOrdersInSystemThenReturnNull() throws NoCustomerException, OrderQueue.NoPurchasesException{
     OrderQueue orderQueue= new OrderQueue();
-    Order order = new Order ("SomeValues","OtherValues");
-    order.addPurchase(new Purchase("SomeID",12)) ;
-    orderQueue.add(order);
-    Order order2 = new Order ("SomeValues","OtherValues");
-    order2.addPurchase(new Purchase("SomeID",12)) ;
-    orderQueue.add(order2);
     
     Order result = orderQueue.next(); 
-    assertEquals (result,order);
-    assertNull(result.getTimeProcessed());
+   
+    assertNull(result);
     
     
     
