419053cd947e4c7da221a1612ff19d03468ed30a
wayne <wayne@wayne-PC>
2015-05-30 00:51:29 -0400

    All without Json

--- src/cpd4414/assign2/OrderQueue.java
+++ src/cpd4414/assign2/OrderQueue.java
@@ -17,8 +17,11 @@
 
 package cpd4414.assign2;
 
+import static java.lang.Integer.parseInt;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -27,7 +30,7 @@ import java.util.Queue;
  */
 public class OrderQueue {
     Queue<Order> orderQueue = new ArrayDeque<>();
-    
+    List<Order> orderList = new ArrayList<>();
     public void add(Order order) throws NoCustomerException,  NoPurchasesException{
         if (order.getCustomerId().isEmpty()&&order.getCustomerName().isEmpty()){
         
@@ -39,16 +42,74 @@ public class OrderQueue {
         }
       
         orderQueue.add(order);
-        order.setTimeReceived(new Date());
+        order. setTimeReceived(new Date());
     }
+
+    void process(Order next) throws NoTimeReceivedException {
+       if (next.equals(next())){
+          /* boolean isOkay = true;
+         
+           for(Purchase p : next.getListOfPurchases()){
+               if( Inventory.getQuantityForId(parseInt(p.getProductId())) < p.getQuantity()){
+                   isOkay=false;
+               
+               }
+           }
+           if(isOkay){*/
+                  orderList.add(orderQueue.remove());
+                    next.setTimeProcessed(new Date());}
+       
+      // }
+       else if (next.getTimeReceived()==null){
+                throw new NoTimeReceivedException();
+                }
+    }
+
+    void fulfill(Order next) throws NoTimeReceivedException, NoTimeProcessedException {
+      if(next.getTimeReceived()==null){
+            throw new NoTimeReceivedException();
+      }
+      if(next.getTimeProcessed()==null){
+            throw new NoTimeProcessedException();
+      }
+        
+        
+        if(orderList.contains(next))
+         {
+            next.setTimeFulfilled(new Date());
+          }
+    }
+
+    String report() {
+       String output = "";
+        return null;
+    }
+
+ 
+    
+
+ 
     public class NoCustomerException extends Exception{
-      public NoCustomerException(){
-       super("No Customer Provided");
-    }
+            public NoCustomerException(){
+                 super("No Customer Provided");
+              }
     }
      public class NoPurchasesException extends Exception{
       public NoPurchasesException(){
        super("No Purchases Provided");
     }
     }
+     public class NoTimeReceivedException extends Exception{
+      public NoTimeReceivedException(){
+       super("No Time received");
+    }
+    }
+       public class NoTimeProcessedException extends Exception{
+      public NoTimeProcessedException(){
+       super("No Time Processed");
+    }
+    }
+     public Order next(){
+     return orderQueue.peek();
+     }
 }
